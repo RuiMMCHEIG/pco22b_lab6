9f5d771d7397d38c3ff930279d12d034f30a102c
act-reds <arzur.cateltorres@heig-vd.ch>
2022-12-06 08:57:28 +0100

    rebase

--- pco_rendu.sh
+++ pco_rendu.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
-SCRIPT=${BASH_SOURCE[0]}
-SCRIPTPATH=$(realpath $(dirname $SCRIPT))
 PROJECT_DIR=code
-PROJECT_FILE=QtrainSimStudent.pro
-PROG1_DIR=${PROJECT_DIR}/prog1
-PROG2_DIR=${PROJECT_DIR}/prog2
-REPORT_FILE=rendu.pdf
+REPORT_FILE=rapport.pdf
+PROJECT_FILE=Lab3_Factory.pro
 ARCHIVE=rendu.tar.gz
 
-cd $SCRIPTPATH
-
-if [ ! -f "${PROJECT_DIR}/${PROJECT_FILE}" ]
+if [ ! -d "$PROJECT_DIR" ]
 then
-    echo "Could not find project file : $PROJECT_FILE in $(realpath $PROJECT_DIR)" >&2
+    echo "Could not find $PROJECT_DIR directory in $(pwd)" >&2
     exit 1
 fi
 
 if [ ! -f "$REPORT_FILE" ]
 then
-    echo "Could not find $REPORT_FILE directory in $SCRIPTPATH" >&2
+    echo "Could not find $REPORT_FILE directory in $(pwd)" >&2
+    exit 1
+fi
+
+if [ ! -f "$PROJECT_DIR/$PROJECT_FILE" ]
+then
+    echo "Could not find project file : $PROJECT_FILE in $(realpath $PROJECT_DIR)" >&2
     exit 1
 fi
 
 echo "The following files are archived in $ARCHIVE : "
-tar --exclude="$ARCHIVE" --exclude='*.o' --exclude='*.user' -czvf $ARCHIVE ${PROJECT_DIR}/${PROJECT_FILE} $PROG1_DIR $PROG2_DIR $REPORT_FILE
+tar --exclude='rendu.tar.gz' --exclude='*.o' --exclude='*.user' -czvf $ARCHIVE code $REPORT_FILE
